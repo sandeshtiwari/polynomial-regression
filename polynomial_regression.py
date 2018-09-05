d81565c54dd817eea03f0b5e47a547e1c9afe1ae
sandesh <sandeshtiwari@live.com>
2018-09-05 13:54:50 -0500

    feat: Fit the transformed matrix to a linear model

--- polynomial_regression.py
+++ polynomial_regression.py
@@ -31,3 +31,5 @@ lin_reg.fit(X, y)
 from sklearn.preprocessing import PolynomialFeatures
 poly_reg = PolynomialFeatures(degree=2)
 X_poly = poly_reg.fit_transform(X)
+lin_reg_2 = LinearRegression()
+lin_reg_2.fit(X_poly, y)
