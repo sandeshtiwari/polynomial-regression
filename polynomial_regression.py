cc9d30235a9ae10d7045679aa19b03e55dec2ec8
sandesh <sandeshtiwari@live.com>
2018-09-05 15:31:40 -0500

    feat: Visualizing the polynomial model and change its degree

--- polynomial_regression.py
+++ polynomial_regression.py
@@ -29,7 +29,7 @@ lin_reg.fit(X, y)
 
 # Fitting Polynomial Regression to the dataset
 from sklearn.preprocessing import PolynomialFeatures
-poly_reg = PolynomialFeatures(degree=2)
+poly_reg = PolynomialFeatures(degree=3)
 X_poly = poly_reg.fit_transform(X)
 lin_reg_2 = LinearRegression()
 lin_reg_2.fit(X_poly, y)
@@ -40,6 +40,15 @@ plt.scatter(X, y, color="red")
 # Visualising the Linear Regression results
 plt.plot(X, lin_reg.predict(X), color='blue')
 plt.title("Truth or Bluff(Linear Regression)")
+plt.xlabel("Position level")
+plt.ylabel("Salary")
+plt.show()
+
+# Visualising the real data
+plt.scatter(X, y, color="red")
+# Visualising the Polynomial Regression results
+plt.plot(X, lin_reg_2.predict(poly_reg.fit_transform(X)), color='blue')
+plt.title("Truth or Bluff(Polynomial Regression)")
 plt.xlabel("Position level")
 plt.ylabel("Salary")
 plt.show()
