820f7e3ce26d77efa1ea1894f612f7d038670172
Sandesh Tiwari <sandeshtiwari@live.com>
2018-09-06 22:13:15 -0500

    feat: Predict using polynomila regression model

--- polynomial_regression.py
+++ polynomial_regression.py
@@ -58,6 +58,7 @@ plt.show()
 
 # Predicting a new result with the Linear Regression
 lin_reg.predict(6.5)
-
+# Predicting a new result with the Polynomial Regression
+lin_reg_2.predict(poly_reg.fit_transform(6.5))
 
 
